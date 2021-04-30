0ed9c980dd5ddc6ee1c932161d2da23ccfdef6d8
Jorge196 <JoRamirez96@gmail.com>
2021-04-30 14:46:04 -0400

    added posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:image_url)
+    end
+end
