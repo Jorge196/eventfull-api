5d5ea9867119fbd3fb9016f6b434b1565581b430
Jorge196 <JoRamirez96@gmail.com>
2021-07-07 20:26:19 -0400

    added validations to each model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user 
     belongs_to :post
+
+    validates :description, presence: true 
+    validates :name, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
     has_one_attached :picture
     has_many :comments 
 
+    validates :name, presence: true
+
     def picture_url 
         Rails.application.routes.url_helpers.url_for(picture) if picture.attached?
     end 
